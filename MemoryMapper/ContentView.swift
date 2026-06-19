ce36fcbb531633ea88db383d906ee2e66de5a3ce
Priyanshu0007 <priyanshu2859@gmail.com>
2026-06-19 12:00:00 +0530

    Initial Commit

---
+++ MemoryMapper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MemoryMapper
+//
+//  Created by Priyanshu Gupta on 30/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
