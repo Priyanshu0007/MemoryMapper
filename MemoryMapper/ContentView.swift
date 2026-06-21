687539483485377cf80603f718066dd42f027f8f
Priyanshu0007 <priyanshu2859@gmail.com>
2026-06-21 12:00:00 +0530

    feat: add modal sheet to capture custom titles for new map markers

--- MemoryMapper/ContentView.swift
+++ MemoryMapper/ContentView.swift
@@ -16,9 +16,13 @@ struct Memory: Identifiable{
 }
 
 struct ContentView: View {
+    @State private var isShowingSheet: Bool = false;
+    @State private var tempCoordinate: CLLocationCoordinate2D?
+    @State private var newMemoryTitle: String = ""
+    
     @State private var position: MapCameraPosition = .automatic;
     @State private var memories: [Memory] = [
-        Memory(title: "Apple Park", coordinate:CLLocationCoordinate2D(latitude: 37.3346, longitude: -122.0090))
+        Memory(title: "Zaika", coordinate:CLLocationCoordinate2D(latitude: 28.4164, longitude: 77.0368))
     ]
     var body: some View {
         MapReader{ reader in
@@ -28,11 +32,31 @@ struct ContentView: View {
                 }
             }.onTapGesture(coordinateSpace: .local) { location in
                 if let coordinate = reader.convert(location, from: .local){
-                    let newMemory = Memory(title: "New Memory", coordinate: coordinate)
-                    memories.append(newMemory)
+                    tempCoordinate =  coordinate
+                    newMemoryTitle = ""
+                    isShowingSheet = true
                 }
             }
-        }.ignoresSafeArea()
+        }
+        .ignoresSafeArea()
+        .sheet(isPresented: $isShowingSheet){
+            VStack(spacing: 20){
+                Text("Name This Place").font(.headline)
+                TextField("Eg: Great Coffee Shop", text: $newMemoryTitle)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.horizontal)
+                Button("Save Marker"){
+                    if let coordinate = tempCoordinate{
+                        let newMemory = Memory(title: newMemoryTitle, coordinate: coordinate)
+                        memories.append(newMemory)
+                        isShowingSheet = false
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                .disabled(newMemoryTitle.trimmingCharacters(in: .whitespaces).isEmpty)
+                Spacer();
+            }.padding(.top,30).presentationDetents([.medium])
+        }
     }
 }
 
