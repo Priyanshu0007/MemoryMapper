5a727956f4ad176339b22066a2e028d0fb3785af
Priyanshu0007 <priyanshu2859@gmail.com>
2026-06-30 23:40:44 +0530

    Initial Commit

---
+++ MemoryMapper/MemoryMapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryMapperApp.swift
+//  MemoryMapper
+//
+//  Created by Priyanshu Gupta on 30/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MemoryMapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
